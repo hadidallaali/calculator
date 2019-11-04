81dc29831c12526279f7d6e239185bd1700424cb
hadidallaali <hadi.dallaali@gmail.com>
2019-11-04 16:52:43 -0500

    Implemented different operations

--- main.cpp
+++ main.cpp
@@ -1,19 +1,39 @@
 #include <iostream>
 using namespace std;
 
-double addTwoNumbers(double num1, double num2)
+double simpleOperations(double num1, char oprtr,double num2)
 {
-	return num1 + num2;
+	double sum;
+// Switch statement identifiying which operation is being used and acting accordingly 
+	switch (oprtr)
+	{
+	case '+':
+		sum = num1 + num2;
+		break;
+	case '-':
+		sum = num1 - num2;
+		break;
+	case '*':
+		sum = num1 * num2;
+		break;
+	case '/':
+		sum = num1 / num2;
+		break;
+
+	}
+	return sum;
 }
 
 int main()
 {
-	double userInput1, userInput2, sum;
+	double userInput1, userInput3;
+	char userInput2;
 		
-	cout << "Input 2 numbers to add:";
-	cin >> userInput1 >> userInput2;
+	cout << "Input 2 numbers to add,subtract,multiply or divide:";
+	cin >> userInput1 >> userInput2>>userInput3;
 
-	cout << addTwoNumbers(userInput1, userInput2);
+	cout << simpleOperations(userInput1, userInput2, userInput3);
 	
+
 	return 0;
 }
