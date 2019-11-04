84a666b80c570df1629f385088d93436d25b467a
hadidallaali <hadi.dallaali@gmail.com>
2019-11-03 19:37:32 -0500

    Cleaned up the code to be more efficient

--- main.cpp
+++ main.cpp
@@ -3,9 +3,7 @@ using namespace std;
 
 double addTwoNumbers(double num1, double num2)
 {
-	double sum;
-	sum = num1 + num2;
-	return sum;
+	return num1 + num2;
 }
 
 int main()
@@ -15,8 +13,7 @@ int main()
 	cout << "Input 2 numbers to add:";
 	cin >> userInput1 >> userInput2;
 
-	sum = addTwoNumbers(userInput1, userInput2);
-	cout << sum;
-
+	cout << addTwoNumbers(userInput1, userInput2);
+	
 	return 0;
 }
